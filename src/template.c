80aa310496a8c053ab36b6f7e141234e4f1912c5
JaniOllila <jani.ollilaa@gmail.com>
2025-10-29 20:49:20 +0200

    imu_init

--- src/template.c
+++ src/template.c
@@ -13,8 +13,28 @@
 // Default stack size for the tasks. It can be reduced to 1024 if task is not using lot of memory.
 #define DEFAULT_STACK_SIZE 2048 
 
+struct imu_data {
+    float ax;
+    float ay;
+    float az;
+    float gx;
+    float gy;
+    float gz;
+    float t;
+};
+
+struct imu_data imuData {
+    imuData.ax = 0.0;
+    imuData.ay = 0.0;
+    imuData.az = 0.0;
+    imuData.gx = 0.0;
+    imuData.gy = 0.0;
+    imuData.gz = 0.0;
+    imuData.t = 0.0;
+}
+
 //Add here necessary states
-enum state { IDLE=1 };
+enum state { IDLE=1, READ_DATA, UPDATE_DATA};
 enum state programState = IDLE;
 
 static void example_task(void *arg){
@@ -25,6 +45,39 @@ static void example_task(void *arg){
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
+
+static void imu_task(void *pvParameters) {
+    (void)pvParameters;
+    if (programState == READ_DATA) {
+    // Creating a pointer to imu_data structure
+    struct imu_data *data = &imuData;
+
+    // Setting up the sensor. 
+    if (init_ICM42670() == 0) {
+        printf("ICM-42670P initialized successfully!\n");
+        if (ICM42670_start_with_default_values() != 0){
+            printf("ICM-42670P could not initialize accelerometer or gyroscope");
+        }
+    } else {
+        printf("Failed to initialize ICM-42670P.\n");
+    }
+    // Start collection data here. Infinite loop. 
+    while (1)
+    {
+        if (ICM42670_read_sensor_data(data->ax, data->ay, data->az, data->gx, data->gy, data->gz, data->t) == 0) {
+            
+            printf("Accel: X=%f, Y=%f, Z=%f | Gyro: X=%f, Y=%f, Z=%f| Temp: %2.2f°C\n", data->ax, data->ay, data->az, 
+                data->gx, data->gy, data->gz, data->t);
+            
+        } else {
+            printf("Failed to read imu data\n");
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+    }
+}
+
+
 
 int main() {
     stdio_init_all();
